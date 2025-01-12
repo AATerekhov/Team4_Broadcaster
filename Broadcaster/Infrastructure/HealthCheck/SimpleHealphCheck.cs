0dd6e348db1e2bc7a73dda563d06024c35a2445d
AATerekhov <A.A.Terekhov@mail.ru>
2025-01-12 19:46:21 +0300

    Adding healph checks

---
+++ Broadcaster/Infrastructure/HealthCheck/SimpleHealphCheck.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.Diagnostics.HealthChecks;
+
+namespace Broadcaster.Infrastructure.HealthCheck
+{
+    public class SimpleHealphCheck : IHealthCheck
+    {
+        public async Task<HealthCheckResult> CheckHealthAsync(HealthCheckContext context, CancellationToken cancellationToken = default)
+        {
+            return new HealthCheckResult(HealthStatus.Healthy);
+        }
+    }
+}
